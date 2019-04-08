a01e49f1b4f211ae3ec602aa43fca8195e80b507
Zoran-Petrov <zoran_782002@yahoo.com>
2019-04-08 18:10:54 +0300

    extending Application

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        System.out.println("Danger!");
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
